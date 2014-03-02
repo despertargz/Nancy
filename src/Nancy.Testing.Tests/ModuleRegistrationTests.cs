b058983c0a1802d4075cc37f5b126f2269e7644e
despertargz <mev412@gmail.com>
2014-03-02 02:52:33 -0600

    Added test for module registration. This demonstrates the correct behavior that if I register TestModule with the container, the container should be able to produce the registered TestModule when the module is resolved later by the framework.

---
+++ src/Nancy.Testing.Tests/ModuleRegistrationTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Nancy.Testing.Tests
+{
+    public class ModuleRegistrationTests 
+    {
+        [Fact]
+        public void test_module_resolves_from_previous_container_registration()
+        {
+            Browser browser = new Browser(new ModuleRegistrationBootStrap());
+            string responseText = browser.Get("/test").Body.AsString();
+            Assert.Equal("return value", responseText);
+        }
+    }
+
+    public class ModuleRegistrationBootStrap : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureApplicationContainer(TinyIoc.TinyIoCContainer container)
+        {
+            base.ConfigureApplicationContainer(container);
+            container.Register<TestModule>(new TestModule("return value"));
+        }
+    }
+
+    public class TestModule : NancyModule
+    {
+        public TestModule(string returnValue)
+        {
+            Get["/test"] = _ => returnValue;
+        }
+    }
+}
